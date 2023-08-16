7afba0e10cc2d48a7733307cd3c90a42ed9bd678
Alejandro Ahijado Ibarra <alejandro.ahijado@gmail.com>
2023-08-16 16:37:56 +0200

    Added points to the gameover screen

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ let lives = 3;
 let ammo = 10;
 class Player {
     constructor(){
-        this.width = 5;
-        this.height = 10;
-        this.x = 50 - (this.width / 2);
+        this.width = 50;
+        this.height = 70;
+        this.x = 900 - (this.width / 2);
         this.y = 0;
         this.domElement = null;
-
-
+        this.boardWidth = 1920;
+        this.boardHeight = 900;
         this.createDomElement();
         this.addEventListeners();
     }
@@ -19,32 +19,38 @@ class Player {
         this.domElement = document.createElement("div");
 
         this.domElement.id = "player";
-        this.domElement.style.width = this.width + "vw";
-        this.domElement.style.height = this.height + "vh";
-        this.domElement.style.left = this.x + "vw";
-        this.domElement.style.bottom = this.y + "vh";
+        this.domElement.style.width = this.width + "px";
+        this.domElement.style.height = this.height + "px";
+        this.domElement.style.left = this.x + "px";
+        this.domElement.style.bottom = this.y + "px";
 
         const parentElement = document.getElementById("board");
         parentElement.appendChild(this.domElement);
     }
     
     moveLeft(){
-        this.x -= 5;
-        this.domElement.style.left = this.x + "vw";
+        this.x -= 20;
+        if (this.x > 0) {
+            this.domElement.style.left = this.x + "px";
+        }
+        
     }
     moveRight(){
-        this.x += 5;
-        this.domElement.style.left = this.x + "vw";
+        this.x += 20;
+        this.domElement.style.left = this.x + "px";
     }
 
     moveUp(){
-        this.y += 5;
-        this.domElement.style.bottom = this.y + "vh";
+        this.y += 20;
+        this.domElement.style.bottom = this.y + "px";        
     }
 
     moveDown(){
-        this.y -= 5;
-        this.domElement.style.bottom = this.y + "vh";
+        this.y -= 20;
+        if(this.y < this.boardHeight - this.y){
+            this.domElement.style.bottom = this.y + "px";
+        }
+        
     }
 
     shooting() {
@@ -82,10 +88,10 @@ const player = new Player ();
 
 class Enemy {
     constructor() {
-        this.width = 5;
-        this.height = 5;
-        this.x = Math.floor(Math.random() * (100 - this.width + 1));
-        this.y = 100;
+        this.width = 50;
+        this.height = 30;
+        this.x = Math.floor(Math.random() * (900 - this.width + 1));
+        this.y = 900;
         this.domElement = null;
         this.createDomElement();
     }
@@ -93,18 +99,18 @@ class Enemy {
     createDomElement() {
         this.domElement = document.createElement("div");
         this.domElement.id = "enemy";
-        this.domElement.style.width = this.width + "vw";
-        this.domElement.style.height = this.height + "vh";
-        this.domElement.style.left = this.x + "vw";
-        this.domElement.style.bottom = this.y + "vh";
+        this.domElement.style.width = this.width + "px";
+        this.domElement.style.height = this.height + "px";
+        this.domElement.style.left = this.x + "px";
+        this.domElement.style.bottom = this.y + "px";
 
         const parentElement = document.getElementById("board");
         parentElement.appendChild(this.domElement);
     }
 
     moveDown() {
-        this.y -= 0.5;
-        this.domElement.style.bottom = this.y + "vh";
+        this.y -= 5;
+        this.domElement.style.bottom = this.y + "px";
     }
 }
 
@@ -124,12 +130,85 @@ function createEnemies (){
                 lives--; 
                 livesDisplay.textContent = `Lives: ${lives}`;
                 if (lives === 0) {
-                    location.href = "./gameover.html";
+                    location.href = `./gameover.html?score=${points}`;
                 }
             }
         });
     }, 100);
-    bulletCollision = setInterval(() => {
+}
+
+createEnemies();
+
+
+function createPointsDisplay() {
+    const pointsDisplay = document.createElement("div");
+    pointsDisplay.id = "points-display";
+    pointsDisplay.textContent = `Points: ${points}`;
+        
+    const parentElement = document.getElementById("board");
+    parentElement.appendChild(pointsDisplay);
+    return pointsDisplay;
+}
+const pointsDisplay = createPointsDisplay();
+
+function createLivesDisplay() {
+    const livesDisplay = document.createElement("div");
+    livesDisplay.id = "lives-display";
+    livesDisplay.textContent = `Lives: ${lives}`;
+        
+    const parentElement = document.getElementById("board");
+    parentElement.appendChild(livesDisplay);
+    return livesDisplay;
+}
+const livesDisplay = createLivesDisplay();
+
+function createAmmoDisplay() {
+    const ammoDisplay = document.createElement("div");
+    ammoDisplay.id = "ammo-display";
+    ammoDisplay.textContent = `Ammo: ${ammo}`;
+        
+    const parentElement = document.getElementById("board");
+    parentElement.appendChild(ammoDisplay);
+    return ammoDisplay;
+}
+const ammoDisplay = createAmmoDisplay();
+
+setInterval(() => {
+    function collision() {
+        enemiesArr.forEach(function(enemy, index){
+            if (
+                player.x < enemy.x + enemy.width &&
+                player.x + player.width > enemy.x &&
+                player.y < enemy.y + enemy.height &&
+                player.y + player.height > enemy.y
+            ) {
+                lives--; 
+                livesDisplay.textContent = `Lives: ${lives}`;
+                if (lives === 0) {
+                    
+                    location.href = `./gameover.html?score=${points}`;
+                }
+                enemy.domElement.remove();
+                enemiesArr.splice(index, 1);
+            }
+        })
+    };
+    collision();
+}, 100); 
+
+
+
+
+
+bulletInterval = setInterval(() => {
+    bulletsArr.forEach((bullet, i) => {
+        bullet.y += 100
+        bullet.domElement.style.bottom = bullet.y + "px"
+        if (bullet.y > 900) {
+            bulletsArr.splice(i, 1)
+        }
+    });
+    function bulletCollision() {
         enemiesArr.forEach((enemy, enemyIndex) => {
             bulletsArr.forEach((bullet, bulletIndex) => {
                 if (
@@ -143,110 +222,43 @@ function createEnemies (){
                     bullet.domElement.remove();
                     bulletsArr.splice(bulletIndex, 1);
                     points = points + 10
-                    if (pointsDisplay){
-                        pointsDisplay.textContent = `Points: ${points}`;
-                    }
-                    if (livesDisplay) {
-                        livesDisplay.textContent = `Lives: ${lives}`;
-                    }
-                    if (ammoDisplay) {
-                        ammoDisplay.textContent = `Ammo: ${ammo}`;
-                    }
+                        if (pointsDisplay){
+                            pointsDisplay.textContent = `Points: ${points}`;
+                        }
+                        if (livesDisplay) {
+                            livesDisplay.textContent = `Lives: ${lives}`;
+                        }
+                        if (ammoDisplay) {
+                            ammoDisplay.textContent = `Ammo: ${ammo}`;
+                        }
                     ammo = ammo + 20;
                     ammoDisplay.textContent = `Ammo: ${ammo}`;
             }});
         });
-    }, 100);
-
-    function createPointsDisplay() {
-        const pointsDisplay = document.createElement("div");
-        pointsDisplay.id = "points-display";
-        pointsDisplay.textContent = `Points: ${points}`;
-        
-        const parentElement = document.getElementById("board");
-        parentElement.appendChild(pointsDisplay);
-        return pointsDisplay;
     }
-    const pointsDisplay = createPointsDisplay();
+    bulletCollision();
 
-    function createLivesDisplay() {
-        const livesDisplay = document.createElement("div");
-        livesDisplay.id = "lives-display";
-        livesDisplay.textContent = `Lives: ${lives}`;
-        
-        const parentElement = document.getElementById("board");
-        parentElement.appendChild(livesDisplay);
-        return livesDisplay;
-    }
-    const livesDisplay = createLivesDisplay();
-
-    function createAmmoDisplay() {
-        const ammoDisplay = document.createElement("div");
-        ammoDisplay.id = "ammo-display";
-        ammoDisplay.textContent = `Ammo: ${ammo}`;
-        
-        const parentElement = document.getElementById("board");
-        parentElement.appendChild(ammoDisplay);
-        return ammoDisplay;
-    }
-    const ammoDisplay = createAmmoDisplay();
-}
-
-createEnemies();
-
-function collision() {
-    setInterval(() => {
-        enemiesArr.forEach(function(enemy){
-            if (
-                player.x < enemy.x + enemy.width &&
-                player.x + player.width > enemy.x &&
-                player.y < enemy.y + enemy.height &&
-                player.y + player.height > enemy.y
-            ) {
-                lives--; 
-                livesDisplay.textContent = `Lives: ${lives}`;
-                if (lives === 0) {
-                    location.href = "./gameover.html";
-                }
-            }
-        }, 1);
-        })  
-};
-
-collision();
-
-
-
-bulletInterval = setInterval(() => {
-    bulletsArr.forEach((bullet, i) => {
-        bullet.y += 5
-        bullet.domElement.style.bottom = bullet.y + "vh"
-        if (bullet.y > 100) {
-            bulletsArr.splice(i, 1)
-        }
-    })
 }, 100)
 
 class Bullet {
     constructor(x, y) {
-        this.width = 1;
-        this.height = 1;
+        this.width = 5;
+        this.height = 20;
         this.x = x
         this.y = y
         this.domElement = null;
         
         this.createDomElement();
-        
     }
 
     createDomElement() {
         this.domElement = document.createElement("div");
         this.domElement.id = "bullet";
-        this.domElement.style.width = this.width + "vw";
-        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.width = this.width + "px";
+        this.domElement.style.height = this.height + "px";
         this.domElement.style.backgroundColor = "red"
-        this.domElement.style.left = this.x + "vw";
-        this.domElement.style.bottom = this.y + "vh";
+        this.domElement.style.left = this.x + "px";
+        this.domElement.style.bottom = this.y + "px";
         console.log(this.domElement)
         const parentElement = document.getElementById("board");
         parentElement.appendChild(this.domElement);
